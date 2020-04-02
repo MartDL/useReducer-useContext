9b625f47f6375ba7bbd17d41f5c83c060d421bac
unknown <martlord81@hotmail.com>
2020-04-02 11:03:17 +0100

    complex state and action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Counter1 from './components/Counter1'
+import Counter2 from './components/Counter2'
+
 
 import './App.css';
 
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       <h2>useReducer examples</h2>
       <Counter1 />
+      <Counter2 />
     </div>
   );
 }
